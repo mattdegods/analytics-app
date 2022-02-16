b4a21fd7d024e1ccfafa3d58e44b713da0f58033
Matt Pfeifer <matthewcpfeifer@gmail.com>
2022-02-15 20:44:59 -0500

    initial work

---
+++ pages/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Image from 'next/image';
+
+function Header() {
+  return (
+    <div className='p-5 shadow-md bg-slate-800'>
+      <h1>Header</h1>
+      {/* left */}
+      
+      {/* center */}
+
+      {/* right */}
+
+    </div>
+  )
+}
+
+export default Header
